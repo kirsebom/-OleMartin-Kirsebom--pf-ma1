e7c5192ea4eb1877a7c1c79cba5dfe4495417802
kirsebom <olemartinkirsebom@gmail.com>
2020-09-18 09:26:20 +0200

    This is my first javascript file

---
+++ script.js
@@ -0,0 +1,31 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Ole-Martin";
+
+// question 3
+var number = 28;
+
+//question 4
+var division = 20 / 5;
+
+// question 5
+var animal = "frog";
+typeof animal;
+console.log(typeof animal);
+
+// question 6
+var orderHasShipped = true;
+
+//question 7
+if (orderHasShipped === true) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+
+//question 8
+for (var i = 0; i < 10; i++) {
+	console.log(i);
+}
